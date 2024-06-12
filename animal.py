01ca43b340a5c0528e2486a0c3f612bca3047f3e
Tim_Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-12 13:18:27 -0500

    Addes Reptile subclass

--- animal.py
+++ animal.py
@@ -3,6 +3,14 @@ class Animal:
         self.name = name
         self.species = species
 class Mammal(Animal):
-    def __init__(self,name,give_birth):
+    def __init__(self,name, species, give_birth):
         super().__init__(name, species = 'Mammal')
         self.give_birth = give_birth
+class Bird(Animal):
+    def __init__(self, name, species,  wingspan):
+        super().__init__(name, species = 'Bird')
+        self.wingspan = wingspan
+class Reptile(Animal):
+    def __init__(self, name, back_in_sun):
+        super().__init__(self, name, species = 'Reptile')
+        self.back_in_sun = back_in_sun
