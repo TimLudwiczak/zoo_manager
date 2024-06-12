847d7228b6469ee8147eacfd1e36aef17284b64e
Tim_Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-12 12:58:28 -0500

    New subclass Mammal

--- animal.py
+++ animal.py
@@ -2,3 +2,7 @@ class Animal:
     def __init__(self, name, species):
         self.name = name
         self.species = species
+class Mammal(Animal):
+    def __init__(self,name,give_birth):
+        super().__init__(name, species = 'Mammal')
+        self.give_birth = give_birth
